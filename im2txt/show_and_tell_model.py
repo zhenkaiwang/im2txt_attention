5175043cd37ff3ce1a909bf81611aafe1d9ad2f2
Zhenkai Wang <Zack@rescomp-16-340946.stanford.edu>
2017-03-11 15:55:14 -0800

    ::Fire:: add rnn_cell_ops

--- im2txt/show_and_tell_model.py
+++ im2txt/show_and_tell_model.py
@@ -29,6 +29,8 @@ import tensorflow as tf
 from im2txt.ops import image_embedding
 from im2txt.ops import image_processing
 from im2txt.ops import inputs as input_ops
+#RNNCELL
+from im2txt.ops import rnn_cell_ops
 
 
 class ShowAndTellModel(object):
@@ -284,10 +286,12 @@ class ShowAndTellModel(object):
     # modified LSTM in the "Show and Tell" paper has no biases and outputs
     # new_c * sigmoid(o).
     # lstm_cell = tf.contrib.rnn.BasicLSTMCell(
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(
+    # lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(
+    lstm_cell = rnn_cell_ops.BasicLSTMCell(
         num_units=self.config.num_lstm_units, state_is_tuple=True)
     if self.mode == "train":
-      lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
+      # lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
+      lstm_cell = rnn_cell_ops.DropoutWrapper(
           lstm_cell,
           input_keep_prob=self.config.lstm_dropout_keep_prob,
           output_keep_prob=self.config.lstm_dropout_keep_prob)
