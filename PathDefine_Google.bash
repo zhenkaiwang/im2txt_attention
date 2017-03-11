44a12f33066aa5fca915ff5655b83a4d3cb107eb
Ubuntu <superNLP@cs224n-gpu2.iyrphcvaq5eufi3t34tetmqagd.bx.internal.cloudapp.net>
2017-03-11 22:38:56 +0000

    Initial Commit

---
+++ PathDefine_Google.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+MSCOCO_DIR="/home/superNLP/usb_hdd/cocodata"
+INCEPTION_CHECKPOINT="/home/superNLP/im2txt/data/inception_v3.ckpt"
+MODEL_DIR="/home/superNLP/im2txt/model"
+NLTK_DATA="/home/superNLP/usb_hdd/nltk_data"
+VOCAB_FILE="/home/superNLP/usb_hdd/cocodata/word_counts.txt"
+CHECKPOINT_DIR="/home/superNLP/im2txt/model/pretrained"
