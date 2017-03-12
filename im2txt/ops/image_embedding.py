5f57e653cb8274256cf8f381fb8574aa8e4d4395
Zhenkai Wang <Zack@Zhenkais-MacBook-Pro.local>
2017-03-11 17:29:45 -0800

    ::Fire:: add subnet features

--- im2txt/ops/image_embedding.py
+++ im2txt/ops/image_embedding.py
@@ -186,8 +186,8 @@ def inception_v3(images,
         net, end_points = inception_v3_base(images, final_endpoint=layer,scope=scope)
         # net2, end_points = inception_v3_base(images, final_endpoint="Conv2d_4a_3x3",scope=scope)
         net2=end_points["Conv2d_4a_3x3"]
-        net2 = tf.reshape(net2, [shape2[0].value, -1, shape2[3].value])
         shape2 = net2.get_shape()
+        net2 = tf.reshape(net2, [shape2[0].value, -1, shape2[3].value])
         print(shape2[0])
         print(shape2[3])
         with tf.variable_scope("logits"):
