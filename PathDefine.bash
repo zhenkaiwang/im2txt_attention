1eac8e36af3b28e4a99d5d4fd618083b616c4ecd
Ubuntu <superNLP@cs224n-gpu2.iyrphcvaq5eufi3t34tetmqagd.bx.internal.cloudapp.net>
2017-03-12 22:11:23 +0000

    bla

--- PathDefine.bash
+++ PathDefine.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 MSCOCO_DIR="/home/superNLP/usb_hdd/cocodata"
-INCEPTION_CHECKPOINT="/home/superNLP/im2txt/data/inception_v3.ckpt"
-MODEL_DIR="/home/superNLP/im2txt/model"
+INCEPTION_CHECKPOINT="/home/superNLP/ours/im2txt_attention/data/inception_v3.ckpt"
+MODEL_DIR="/home/superNLP/ours/im2txt_attention/model"
 NLTK_DATA="/home/superNLP/usb_hdd/nltk_data"
 VOCAB_FILE="/home/superNLP/usb_hdd/cocodata/word_counts.txt"
-CHECKPOINT_DIR="/home/superNLP/im2txt/model/train2"
+CHECKPOINT_DIR="/home/superNLP/ours/im2txt_attention/model/train"
