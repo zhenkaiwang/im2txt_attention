c58e91103883b60bfc4626ab1abfd9e042a1a006
Zhenkai Wang <Zack@rescomp-16-340946.stanford.edu>
2017-03-12 14:21:32 -0700

    :fire: shown and tell

--- im2txt/show_and_tell_model.py
+++ im2txt/show_and_tell_model.py
@@ -504,7 +504,7 @@ class ShowAndTellModel(object):
 
         # Run a single LSTM step.
         print(self.seq_embeddings)
-        inputs_wa = get_inputs(self.seq_embeddings,self.image_sub_features)
+        # inputs_wa = get_inputs(self.seq_embeddings,self.image_sub_features)
         
         lstm_outputs, state_tuple = lstm_cell(
             inputs=tf.squeeze(self.seq_embeddings, squeeze_dims=[1]),
@@ -515,7 +515,7 @@ class ShowAndTellModel(object):
         tf.concat(1,state_tuple, name="state")
       else:
         # Run the batch of sequence embeddings through the LSTM.
-        inputs_wa = get_inputs(self.seq_embeddings,self.image_sub_features)
+        # inputs_wa = get_inputs(self.seq_embeddings,self.image_sub_features)
 
         sequence_length = tf.reduce_sum(self.input_mask, 1)
         # lstm_outputs, _ = tf.nn.dynamic_rnn(cell=lstm_cell,
