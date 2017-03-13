9f70f3f5c4736f67c1c6b1c4756f181113c00e8b
Zhenkai Wang <Zack@rescomp-16-340946.stanford.edu>
2017-03-12 20:28:08 -0700

    get_inputs

--- im2txt/show_and_tell_model.py
+++ im2txt/show_and_tell_model.py
@@ -137,6 +137,8 @@ class ShowAndTellModel(object):
 	image_sub_reshaped=tf.reshape(self.image_sub_features,[self.config.batch_size,1,-1])
 	image_sub_tile=tf.tile(image_sub_reshaped,tf.pack([1,shape_seq[1],1]))
 	output=tf.concat(2,[self.seq_embeddings,image_sub_tile])
+	print('sub features shape')
+	print(self.image_sub_features)
 	return output
 
 
