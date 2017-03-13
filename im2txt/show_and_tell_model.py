e430fecad0b6a3463696f7f40398c09afa62f79e
JulieBZ <zbie1994@gmail.com>
2017-03-12 17:33:42 -0700

    **

--- im2txt/show_and_tell_model.py
+++ im2txt/show_and_tell_model.py
@@ -103,26 +103,26 @@ class ShowAndTellModel(object):
 	self.global_step = None
 
   def get_inputs(self):
-	# self.image_sub_features [batch_size, sub_featrue_num, sub_feature_length]
-	# self.seq_embeddings [batch_size, padded_length, embedding_size]
-	batch_size, padded_length, embedding_size = tf.shape(self.seq_embeddings)
-	batch_size, sub_feature_num, sub_feature_length = tf.shape(self.image_sub_features)
+	size1=tf.shape(self.seq_embeddings)
+	padded_length = size1[1]
+	batch_size, sub_feature_num, sub_feature_length = self.image_sub_features.get_shape()
 	# generate RNN input: word feature + local features
-	output = tf.zeros([batch_size, padded_length, embedding_size + sub_feature_length * sub_feature_num], tf.float32)
-	
+	output = tf.zeros([batch_size, padded_length, 
+	embedding_size + sub_feature_num * sub_feature_length], tf.float32)
+	index=0
+	while_condition=lambda i: tf.less(i,padded_length)
+	def body(i):
+		output[img][index] = tf.concat([tf.reshape(self.seq_embeddings[img][pad], [-1]),
+				   tf.reshape(self.image_sub_features[img], [-1])])
+		index=index+1
+		return [tf.add(i,1)]
+
 	for img in range(batch_size):
-		# for each image in batch_size
-		for pad in range(padded_length):
-			output[img][pad] = tf.concat([tf.reshape(self.seq_embeddings[img][pad], [-1]),
-								tf.reshape(self.image_sub_features[img], [-1])])
-	print(self.seq_embeddings.get_shape())
-	print(self.image_sub_features.get_shape())
-	print('output shape:')
-	print(output.get_shape())
-
-
-	#tf.concat([self.seq_embeddings, tf.reshape(self.image_sub_features,[batch_size, 1, -1])], 2)
+		i = tf.constant(0)
+		index=0
+		r=tf.while_loop(while_condition,body,i)
 	return output
+
 
 
   def is_training(self):
@@ -295,121 +295,121 @@ class ShowAndTellModel(object):
 	self.seq_embeddings = seq_embeddings
 
   def build_model(self):
-    """Builds the model.
+	"""Builds the model.
 
-    Inputs:
-      self.image_embeddings
-      self.seq_embeddings
-      self.target_seqs (training and eval only)
-      self.input_mask (training and eval only)
+	Inputs:
+	  self.image_embeddings
+	  self.seq_embeddings
+	  self.target_seqs (training and eval only)
+	  self.input_mask (training and eval only)
 
-    Outputs:
-      self.total_loss (training and eval only)
-      self.target_cross_entropy_losses (training and eval only)
-      self.target_cross_entropy_loss_weights (training and eval only)
-    """
-    # This LSTM cell has biases and outputs tanh(new_c) * sigmoid(o), but the
-    # modified LSTM in the "Show and Tell" paper has no biases and outputs
-    # new_c * sigmoid(o).
-    # lstm_cell = tf.contrib.rnn.BasicLSTMCell(
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(
-    # lstm_cell = rnn_cell_ops.BasicLSTMCell(
-        num_units=self.config.num_lstm_units, state_is_tuple=True)
-    if self.mode == "train":
-      lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
-      # lstm_cell = rnn_cell_ops.DropoutWrapper(
-          lstm_cell,
-          input_keep_prob=self.config.lstm_dropout_keep_prob,
-          output_keep_prob=self.config.lstm_dropout_keep_prob)
+	Outputs:
+	  self.total_loss (training and eval only)
+	  self.target_cross_entropy_losses (training and eval only)
+	  self.target_cross_entropy_loss_weights (training and eval only)
+	"""
+	# This LSTM cell has biases and outputs tanh(new_c) * sigmoid(o), but the
+	# modified LSTM in the "Show and Tell" paper has no biases and outputs
+	# new_c * sigmoid(o).
+	# lstm_cell = tf.contrib.rnn.BasicLSTMCell(
+	lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(
+	# lstm_cell = rnn_cell_ops.BasicLSTMCell(
+		num_units=self.config.num_lstm_units, state_is_tuple=True)
+	if self.mode == "train":
+	  lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
+	  # lstm_cell = rnn_cell_ops.DropoutWrapper(
+		  lstm_cell,
+		  input_keep_prob=self.config.lstm_dropout_keep_prob,
+		  output_keep_prob=self.config.lstm_dropout_keep_prob)
 
-    with tf.variable_scope("lstm", initializer=self.initializer) as lstm_scope:
-      # Feed the image embeddings to set the initial LSTM state.
-      zero_state = lstm_cell.zero_state(
-          batch_size=self.image_embeddings.get_shape()[0], dtype=tf.float32)
-      _, initial_state = lstm_cell(self.image_embeddings, zero_state)
+	with tf.variable_scope("lstm", initializer=self.initializer) as lstm_scope:
+	  # Feed the image embeddings to set the initial LSTM state.
+	  zero_state = lstm_cell.zero_state(
+		  batch_size=self.image_embeddings.get_shape()[0], dtype=tf.float32)
+	  _, initial_state = lstm_cell(self.image_embeddings, zero_state)
 
-      # Allow the LSTM variables to be reused.
-      lstm_scope.reuse_variables()
+	  # Allow the LSTM variables to be reused.
+	  lstm_scope.reuse_variables()
 
-      if self.mode == "inference":
-        # In inference mode, use concatenated states for convenient feeding and
-        # fetching.
-        #tf.concat(initial_state, 1, name="initial_state")
-        tf.concat(1,initial_state, name="initial_state")
+	  if self.mode == "inference":
+		# In inference mode, use concatenated states for convenient feeding and
+		# fetching.
+		#tf.concat(initial_state, 1, name="initial_state")
+		tf.concat(1,initial_state, name="initial_state")
 
-        # Placeholder for feeding a batch of concatenated states.
-        state_feed = tf.placeholder(dtype=tf.float32,
-                                    shape=[None, sum(lstm_cell.state_size)],
-                                    name="state_feed")
-        #state_tuple = tf.split(value=state_feed, num_or_size_splits=2, axis=1)
-        state_tuple = tf.split(split_dim=1,num_split=2,value=state_feed)
+		# Placeholder for feeding a batch of concatenated states.
+		state_feed = tf.placeholder(dtype=tf.float32,
+									shape=[None, sum(lstm_cell.state_size)],
+									name="state_feed")
+		#state_tuple = tf.split(value=state_feed, num_or_size_splits=2, axis=1)
+		state_tuple = tf.split(split_dim=1,num_split=2,value=state_feed)
 
-        # Run a single LSTM step.
-        print(self.seq_embeddings)
-        inputs_wa = self.get_inputs()
-        
-        lstm_outputs, state_tuple = lstm_cell(
-            inputs=tf.squeeze(inputs_wa, squeeze_dims=[1]),
-            state=state_tuple)
+		# Run a single LSTM step.
+		print(self.seq_embeddings)
+		inputs_wa = self.get_inputs()
+		
+		lstm_outputs, state_tuple = lstm_cell(
+			inputs=tf.squeeze(inputs_wa, squeeze_dims=[1]),
+			state=state_tuple)
 
-        # Concatentate the resulting state.
-        #tf.concat(state_tuple, 1, name="state")
-        tf.concat(1,state_tuple, name="state")
-      else:
-        # Run the batch of sequence embeddings through the LSTM.
-        inputs_wa = self.get_inputs()
+		# Concatentate the resulting state.
+		#tf.concat(state_tuple, 1, name="state")
+		tf.concat(1,state_tuple, name="state")
+	  else:
+		# Run the batch of sequence embeddings through the LSTM.
+		inputs_wa = self.get_inputs()
 
-        sequence_length = tf.reduce_sum(self.input_mask, 1)
-        lstm_outputs, _ = tf.nn.dynamic_rnn(cell=lstm_cell,
-                                            inputs=inputs_wa,
-                                            sequence_length=sequence_length,
-                                            initial_state=initial_state,
-                                            dtype=tf.float32,
-                                            scope=lstm_scope)
-        # lstm_outputs, _ = dynamic_rnn(cell=lstm_cell,
-        #                                     inputs=self.seq_embeddings,
-        #                                     sequence_length=sequence_length,
-        #                                     initial_state=initial_state,
-        #                                     dtype=tf.float32,
-        #                                     scope=lstm_scope)        
+		sequence_length = tf.reduce_sum(self.input_mask, 1)
+		lstm_outputs, _ = tf.nn.dynamic_rnn(cell=lstm_cell,
+											inputs=inputs_wa,
+											sequence_length=sequence_length,
+											initial_state=initial_state,
+											dtype=tf.float32,
+											scope=lstm_scope)
+		# lstm_outputs, _ = dynamic_rnn(cell=lstm_cell,
+		#                                     inputs=self.seq_embeddings,
+		#                                     sequence_length=sequence_length,
+		#                                     initial_state=initial_state,
+		#                                     dtype=tf.float32,
+		#                                     scope=lstm_scope)        
 
-    # Stack batches vertically.
-    lstm_outputs = tf.reshape(lstm_outputs, [-1, lstm_cell.output_size])
+	# Stack batches vertically.
+	lstm_outputs = tf.reshape(lstm_outputs, [-1, lstm_cell.output_size])
 
-    with tf.variable_scope("logits") as logits_scope:
-      logits = tf.contrib.layers.fully_connected(
-          inputs=lstm_outputs,
-          num_outputs=self.config.vocab_size,
-          activation_fn=None,
-          weights_initializer=self.initializer,
-          scope=logits_scope)
+	with tf.variable_scope("logits") as logits_scope:
+	  logits = tf.contrib.layers.fully_connected(
+		  inputs=lstm_outputs,
+		  num_outputs=self.config.vocab_size,
+		  activation_fn=None,
+		  weights_initializer=self.initializer,
+		  scope=logits_scope)
 
-    if self.mode == "inference":
-      tf.nn.softmax(logits, name="softmax")
-    else:
-      targets = tf.reshape(self.target_seqs, [-1])
-      weights = tf.to_float(tf.reshape(self.input_mask, [-1]))
+	if self.mode == "inference":
+	  tf.nn.softmax(logits, name="softmax")
+	else:
+	  targets = tf.reshape(self.target_seqs, [-1])
+	  weights = tf.to_float(tf.reshape(self.input_mask, [-1]))
 
-      # Compute losses.
-      losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=targets,
-                                                              logits=logits)
-      batch_loss = tf.div(tf.reduce_sum(tf.multiply(losses, weights)),
-                          tf.reduce_sum(weights),
-                          name="batch_loss")
-      #tf.losses.add_loss(batch_loss)
-      tf.contrib.losses.add_loss(batch_loss)
-      #total_loss = tf.losses.get_total_loss()
-      total_loss = tf.contrib.losses.get_total_loss()
+	  # Compute losses.
+	  losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=targets,
+															  logits=logits)
+	  batch_loss = tf.div(tf.reduce_sum(tf.multiply(losses, weights)),
+						  tf.reduce_sum(weights),
+						  name="batch_loss")
+	  #tf.losses.add_loss(batch_loss)
+	  tf.contrib.losses.add_loss(batch_loss)
+	  #total_loss = tf.losses.get_total_loss()
+	  total_loss = tf.contrib.losses.get_total_loss()
 
-      # Add summaries.
-      tf.summary.scalar("losses/batch_loss", batch_loss)
-      tf.summary.scalar("losses/total_loss", total_loss)
-      for var in tf.trainable_variables():
-        tf.summary.histogram("parameters/" + var.op.name, var)
+	  # Add summaries.
+	  tf.summary.scalar("losses/batch_loss", batch_loss)
+	  tf.summary.scalar("losses/total_loss", total_loss)
+	  for var in tf.trainable_variables():
+		tf.summary.histogram("parameters/" + var.op.name, var)
 
-      self.total_loss = total_loss
-      self.target_cross_entropy_losses = losses  # Used in evaluation.
-      self.target_cross_entropy_loss_weights = weights  # Used in evaluation.
+	  self.total_loss = total_loss
+	  self.target_cross_entropy_losses = losses  # Used in evaluation.
+	  self.target_cross_entropy_loss_weights = weights  # Used in evaluation.
 
   def setup_inception_initializer(self):
 	"""Sets up the function to restore inception variables from checkpoint."""
