292a14c95b81828292ec98398d3cb17d1cb23d22
Zhenkai Wang <Zack@rescomp-16-340946.stanford.edu>
2017-03-13 01:02:29 -0700

    feed encoded image

--- im2txt/ops/rnn_cell_ops.py
+++ im2txt/ops/rnn_cell_ops.py
@@ -317,7 +317,7 @@ class BasicLSTMCell(RNNCell):
 
   def __call__(self, inputs, state, scope=None):
     """Long short-term memory cell (LSTM)."""
-    with vs.variable_scope(scope or type(self).__name__):  # "BasicLSTMCell"
+    with vs.variable_scope(scope or type(self).__name__,reuse=True):  # "BasicLSTMCell"
       # Parameters of gates are concatenated into one multiply for efficiency.
       if self._state_is_tuple:
         c, h = state
