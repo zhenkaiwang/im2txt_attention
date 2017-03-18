784c3ce5ba57b18f45e0a623994afea1ea1061e8
maxiaoba <maxiaoba@umich.edu>
2017-03-17 20:19:54 -0700

    123

--- im2txt/show_and_tell_model.py
+++ im2txt/show_and_tell_model.py
@@ -335,7 +335,7 @@ class ShowAndTellModel(object):
       #image_sub_reshaped=tf.reshape(self.image_sub_features,[1,1,-1])
       image_sub_reshaped=tf.reshape(self.image_sub_features,tf.pack([shape_sub[0],1,-1]))
       #image_sub_tile=tf.tile(image_sub_reshaped,tf.pack([1,shape_seq[1],1]))
-      image_sub_tile=tf.tile(image_sub_reshaped,tf.pack([1,1,1]))
+      image_sub_tile=tf.tile(image_sub_reshaped,tf.pack([shape_seq[0],1,1]))
       print("image_sub_reshaped")
       print(image_sub_reshaped)
       print("image_sub_tile")
