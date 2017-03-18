a686edcb519b2da2ff931ad088a047841fb0b7eb
Ubuntu <superNLP@cs224n-gpu2.iyrphcvaq5eufi3t34tetmqagd.bx.internal.cloudapp.net>
2017-03-18 21:31:04 +0000

    123

--- PathDefine_test.bash
+++ PathDefine_test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-MSCOCO_DIR="/home/superNLP/usb_hdd/cocodata"
+MSCOCO_DIR="/home/superNLP/usb_hdd/cocodata/sub"
 INCEPTION_CHECKPOINT="/home/superNLP/im2txt/data/inception_v3.ckpt"
 MODEL_DIR="/home/superNLP/ours/im2txt_attention/model"
 NLTK_DATA="/home/superNLP/usb_hdd/nltk_data"
